6636505de07860b3072bfd139050b5e474949fe0
shoproizoshlo <iamsusunny@gmail.com>
2023-10-01 10:36:20 +0200

    fixed js code

--- scripts/script.js
+++ scripts/script.js
@@ -11,21 +11,28 @@ fetch("./scripts/data.json")
       bar.style.height = `${item.amount * 2}px`;
 
       // Check if this bar has the maximum amount and change its color
-      if (item.amount === maxAmount) {
-        bar.style.backgroundColor = "hsl(186, 34%, 60%)"; // color for the highest bar
-      } else {
-        bar.style.backgroundColor = "hsl(10, 79%, 65%)"; // Default color
+      function maximumAmount() {
+        if (item.amount === maxAmount) {
+          bar.style.backgroundColor = "hsl(186, 34%, 60%)"; // color for the highest bar
+        } else {
+          bar.style.backgroundColor = "hsl(10, 79%, 65%)"; // Default color
+        }
       }
 
-      bar.addEventListener("mouseover", (event) => {
+      bar.addEventListener("mousemove", (event) => {
         if (item.amount === maxAmount) {
           bar.style.cursor = "pointer";
-          bar.style.backgroundColor = "hsl(186, 34%, 40%)"; // color for the highest bar
+          bar.style.backgroundColor = "hsl(186, 34%, 70%)"; // color for the highest bar
         } else {
           bar.style.cursor = "pointer";
-          bar.style.backgroundColor = "hsl(10, 79%, 45%)"; // Default color
+          bar.style.backgroundColor = "hsl(10, 79%, 75%)"; // Default color
         }
       });
+
+      bar.addEventListener("mouseout", (event) => {
+        maximumAmount();
+      });
+      maximumAmount();
     });
   })
   .catch((error) => console.error("Error loading data:", error));
